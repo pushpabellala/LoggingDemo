a64a42ce79a4aeb90390f317aa0166191e460c8a
pushpa.bellala@gmail.com <pushpa.bellala@gmail.com>
2021-03-21 20:01:07 +0530

    Saved

---
+++ LoggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoggingDemo.Utilities;
+
+namespace LoggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        log4net.ILog logger = log4net.LogManager.GetLogger(typeof(HomeController));  //Declaring Log4Net  
+
+        public ActionResult Index()
+        {
+            Log.Error("Login-page started...");
+            try
+            {
+                int x, y, z;
+                x = 5; y = 0;
+                z = x / y;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
